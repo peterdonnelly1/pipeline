1e0f14a65e6bec82e54a2e38567e7df6ea3c48d2
peterdonnelly1 <pd@red.com.au>
2020-03-19 21:48:13 +1100

    changed tiler subsystem from multithreading to multiprocessing because multithreading was too slow

--- dpcca/tiler_threader.py
+++ dpcca/tiler_threader.py
@@ -1,7 +1,11 @@
 
 import os
-from threading import Thread
+#from threading import Thread
 import multiprocessing
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import wait
+from concurrent.futures import ALL_COMPLETED
+
 
 from tiler_scheduler import tiler_scheduler
 from tiler import tiler
@@ -11,24 +15,21 @@ SUCCESS=False
 FG1="\033[38;5;190m "
 RESET="\033[m"
 
-
 def tiler_threader( args, n_required, stain_norm, norm_method ):
 
   num_cpus = multiprocessing.cpu_count()
 
   if (DEBUG>0):
-    print ( f"TILER_THREADER:          INFO: about to launch {FG1}{num_cpus}{RESET} threads", flush=True ) 
+    print ( f"TILER_THREADER:          INFO: about to launch {FG1}{num_cpus}{RESET} threads", flush=True )
 
-  threads = []
-  for i in range(num_cpus):
-    thr = Thread(target=tiler_scheduler, args=(args, n_required, stain_norm, norm_method, i, num_cpus) )
-    threads.append(thr)
-
-  for t in threads:
-    t.start()
-
-  for t in threads:
-    t.join()
-    
-  return SUCCESS
+  executor = ProcessPoolExecutor(max_workers=num_cpus)
   
+  tasks = []
+  for n in range(0,num_cpus):
+    task=executor.submit( tiler_scheduler, args, n_required, stain_norm, norm_method, n, num_cpus)
+    tasks.append(task)
+ 
+  results = [fut.result() for fut in wait(tasks).done]
+ 
+  
+  return SUCCESS
