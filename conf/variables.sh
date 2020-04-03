b353b4c33e1987dfb4133e950a8484a1baca11a1
peterdonnelly1 <pd@red.com.au>
2020-04-03 17:03:42 +1100

    trivial

--- conf/variables.sh
+++ conf/variables.sh
@@ -49,11 +49,11 @@ elif [[ ${DATASET} == "sarc" ]];
       N_GENES=506
       TILES_PER_IMAGE=200
       NN_TYPE="DENSE"                                                     # supported options are LENET5, VGG11, VGG13, VGG16, VGG19, DENSE, CONV1D, INCEPT3
-      NN_OPTIMIZER="RMSPROP"                                              # supported options are ADAM, ADAMAX, ADAGRAD, SPARSEADAM, ADADELTA, ASGD, RMSPROP, RPROP, SGD, LBFGS
+      NN_OPTIMIZER="ADAM RMSPROP SGD ADADELTA"                                         # supported options are ADAM, ADAMAX, ADAGRAD, SPARSEADAM, ADADELTA, ASGD, RMSPROP, RPROP, SGD, LBFGS
       RANDOM_TILES="True"                                                 # Select tiles at random coordinates from image. Done AFTER other quality filtering
-      N_EPOCHS=10000
-      BATCH_SIZE=32
-      LEARNING_RATE=.001
+      N_EPOCHS=1000
+      BATCH_SIZE="50 40 30 20 10"
+      LEARNING_RATE=.0004
       CLASS_NAMES="dediff_liposarcoma leiomyosarcoma myxofibrosarcoma pleomorphic_MFH synovial undiff_pleomorphic MPNST desmoid giant_cell_MFH"
       STAIN_NORMALIZATION="NONE"                                          # options are NONE, reinhard, spcn  (used in 'save_svs_to_tiles' to specify the type of colour normalization to be performed)
       STAIN_NORM_TARGET="2905cbd1-719b-46d9-b8af-8fe4927bc473/TCGA-FX-A2QS-11A-01-TSA.536F63AE-AD9F-4422-8AC3-4A1C6A57E8D8.svs"
