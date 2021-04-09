eb465d748ca300a7379f81c2e09d504ce3d54350
peterdonnelly1 <pd@red.com.au>
2021-04-09 13:43:25 +1000

    minor changes to the clustering modules - probably GOOD

--- dpcca/sktsne_simple.py
+++ dpcca/sktsne_simple.py
@@ -7,8 +7,6 @@ import seaborn           as sns
 import matplotlib.pyplot as plt
 import matplotlib.colors
 
-import plotly.express as px
-
 # ~ from otsne_utils import plot
 # ~ from otsne_utils import MACOSKO_COLORS
 from sklearn.manifold import TSNE
