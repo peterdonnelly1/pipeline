71aab71e8d4bfa888d3b2863c7e0bd17795cf089
peterdonnelly1 <pd@red.com.au>
2022-03-11 13:48:33 +1100

    increased font size for portrait version of the box plot - SEEMS OK

--- dpcca/trainlenet5.py
+++ dpcca/trainlenet5.py
@@ -5007,12 +5007,11 @@ def box_plot_by_subtype( args, start_time, parameters, writer, total_runs_in_job
     
   hours   = round(( now - start_time ) / 3600,  1   )
   minutes = round(( now - start_time ) /   60,  1   )
-  seconds = round(( now - start_time )       ,  0   )
       
   # Titling
 
   now        = datetime.datetime.now()
-  supertitle = f"{now:%d-%m-%y %H:%M}  Classification of {args.cancer_type_long} Subtypes   ({total_runs_in_job} experiment runs in this box plot.  Total run time {int(hours):02d}:{int(minutes):02d}:{int(seconds):02d})"
+  supertitle = f"{now:%d-%m-%y %H:%M}  Classification of {args.cancer_type_long} Subtypes   ({total_runs_in_job} experiment runs in this box plot.  Total run time {int(hours):02d}:{int(minutes):02d})"
   if args.input_mode=='image':
     title = f"{args.cases[0:25]} ({parameters['n_samples'][0]})  highest class:{args.highest_class_number[0]}  ---  neural network:{parameters['nn_type_image'][0]}  optimizer:{parameters['nn_optimizer'][0]}  epochs:{args.n_epochs}  batch size:{parameters['batch_size'][0]}   \
 held-out:{int(100*parameters['pct_test'][0])}%  lr:{parameters['lr'][0]:<9.6f}  tiles:{parameters['n_tiles'][0]}  tile_size:{parameters['tile_size'][0]}  batch_size:{parameters['batch_size'][0]}  (mags:{mags} probs:{prob})"
@@ -5037,7 +5036,7 @@ lr:{parameters['lr'][0]:<9.6f}  hidden:{parameters['hidden_layer_neurons'][0]}  
   plt.ylabel('subtypes correctly predicted (%)', weight='bold', fontsize=16   )
   plt.yticks(range(0, 100, 10))
   fig.suptitle  ( supertitle, color='dimgray', weight='bold', fontsize=18     ) 
-  ax.set_title  ( title,      color='dimgray',                fontsize=10      )  
+  ax.set_title  ( title,      color='dimgray',                fontsize=10     )  
   ax.set        ( ylim =(0, 100) )
   ax.xaxis.grid ( True, linestyle='dashed', color='lightgrey'  )
   ax.yaxis.grid ( True, linestyle='dotted'                     )
@@ -5052,7 +5051,7 @@ lr:{parameters['lr'][0]:<9.6f}  hidden:{parameters['hidden_layer_neurons'][0]}  
   bp      = plt.boxplot( pct_correct_predictions_plane, labels=labels, vert=True, patch_artist=True, showfliers=True,  medianprops=dict(color="black", alpha=0.7) )
 
   ax.text( x=.55, y=97,  s=f"total predictions made {np.sum(all_predictions_plane):,}, of which correct: {np.sum(correct_predictions_plane):,} ({100*np.sum(correct_predictions_plane)/np.sum(all_predictions_plane):.1f}%)",  horizontalalignment='left', color='dimgray', fontsize=14) 
-  plt.xticks( fontsize=14 )
+  plt.xticks( fontsize=16 )
 
   totals            = total_predictions_by_subtype
   corrects          = correct_predictions_by_subtype
@@ -5062,8 +5061,8 @@ lr:{parameters['lr'][0]:<9.6f}  hidden:{parameters['hidden_layer_neurons'][0]}  
   if (DEBUG>0):
     np.set_printoptions(formatter={ 'float' : lambda x: f"   {CARRIBEAN_GREEN}{x:6.3f}   "} ) 
     print ( f"TRAINLENEJ:       INFO:  headline_correct                           = {MIKADO}{headline_correct}{RESET}",  flush=True )
-    print ( f"TRAINLENEJ:       INFO:  totals                                     = {MIKADO}{totals}{RESET}",  flush=True )
-    print ( f"TRAINLENEJ:       INFO:  corrects                                   = {MIKADO}{corrects}{RESET}",  flush=True )
+    print ( f"TRAINLENEJ:       INFO:  totals                                     = {MIKADO}{totals}{RESET}",            flush=True )
+    print ( f"TRAINLENEJ:       INFO:  corrects                                   = {MIKADO}{corrects}{RESET}",          flush=True )
     print ( f"TRAINLENEJ:       INFO:  best_correct                               = {MIKADO}{best_correct:02d}{RESET}",  flush=True )
     
     
@@ -5079,10 +5078,10 @@ lr:{parameters['lr'][0]:<9.6f}  hidden:{parameters['hidden_layer_neurons'][0]}  
     random   = expected_IFF_random_preds[xtick-1]
     
     ax.text( x=xtick, y=0.75,       s=f"total predictions={total:,}",                     horizontalalignment='center',  color='dimgray',    fontsize=14  ) 
-    ax.text( x=xtick, y=3.5,       s=f"total correct ={correct:,} ({percent:2.1f}%)",    horizontalalignment='center',  color='dimgray',    fontsize=14  )     
-    ax.text( x=xtick, y=6,       s=f"median correct over runs={median:2.1f}%",             horizontalalignment='center',  color='dimgray',    fontsize=14  )    
-    ax.text( x=xtick, y=random+0.5, s=f"expected for random",                       horizontalalignment='center',  color='lightcoral', fontsize=12  )    
-    plt.plot( [xtick-0.27, xtick+0.27], [random, random],                       linewidth=1,     linestyle="--",   color='lightcoral'               )
+    ax.text( x=xtick, y=3.5,        s=f"total correct ={correct:,} ({percent:2.1f}%)",    horizontalalignment='center',  color='dimgray',    fontsize=14  )     
+    ax.text( x=xtick, y=6,          s=f"median correct over runs={median:2.1f}%",         horizontalalignment='center',  color='dimgray',    fontsize=14  )    
+    ax.text( x=xtick, y=random+0.5, s=f"expected for random",                             horizontalalignment='center',  color='lightcoral', fontsize=12  )    
+    plt.plot( [xtick-0.27, xtick+0.27], [random, random],                              linewidth=1,     linestyle="--",   color='lightcoral'               )
 
 
 
