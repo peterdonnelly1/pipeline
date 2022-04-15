79c49ba7a7a318eff7b1f7a3100d5d05ecf28b5b
peterdonnelly1 <pd@red.com.au>
2022-04-15 11:06:12 +1000

    tinkering with combinations of perplexity values for cuda_tsne in cluster_using_ae_output.sh - SEEMS OK

--- cluster_using_ae_outputs.sh
+++ cluster_using_ae_outputs.sh
@@ -312,7 +312,7 @@ elif [[ ${CLUSTERING} == "cuda_tsne" ]]
     #~ ./do_all.sh -d ${DATASET}  -i ${INPUT_MODE}  -t 5000  -x ${N_CLUSTERS}  -X True -s True  -g True  -n classify  -c ${CASES}  -l cuda_tsne  -p 100                                   -u ${USE_AUTOENCODER_OUTPUT}
     #~ ./do_all.sh -d ${DATASET}  -i ${INPUT_MODE}  -t 5000  -x ${N_CLUSTERS}  -X True -s True  -g True  -n classify  -c ${CASES}  -l cuda_tsne  -p 300                                   -u ${USE_AUTOENCODER_OUTPUT}
     #~ ./do_all.sh -d ${DATASET}  -i ${INPUT_MODE}  -t 5000  -x ${N_CLUSTERS}  -X True -s True  -g True  -n classify  -c ${CASES}  -l cuda_tsne  -p 700                                   -u ${USE_AUTOENCODER_OUTPUT}
-    ./do_all.sh -d ${DATASET}  -i ${INPUT_MODE}  -t 5000  -x ${N_CLUSTERS}  -X True -s True  -g True  -n classify  -c ${CASES}  -l cuda_tsne  -p "10 30 70 100 300 700"  -G ${SUPERGRID_SIZE} -u ${USE_AUTOENCODER_OUTPUT}
+    ./do_all.sh -d ${DATASET}  -i ${INPUT_MODE}  -t 5000  -x ${N_CLUSTERS}  -X True -s True  -g True  -n classify  -c ${CASES}  -l cuda_tsne  -p "30 100 300 700"  -G ${SUPERGRID_SIZE} -u ${USE_AUTOENCODER_OUTPUT}
 
 
 elif [[ ${CLUSTERING} == "dbscan" ]]
