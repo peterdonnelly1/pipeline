0cff558293defdcf3b8771b67fe345c59e1c4fdb
peterdonnelly1 <pd@red.com.au>
2022-06-14 13:39:46 +1000

    added code to force cpu affinity to num_cpus in tiler() to get around problem with AMD not using all CPUs - SEEMS OK

--- classi/tiler.py
+++ classi/tiler.py
@@ -61,7 +61,7 @@ def tiler( args, r_norm, n_tiles, top_up_factors, tile_size, batch_size, stain_n
   process   = psutil.Process(pid)
   memoryUse = process.memory_info()[0]/2.**30  # memory use in GB...I think
   affinity  = os.sched_getaffinity(pid)
-  print( f'{SAVE_CURSOR}{RESET}\r\033[{my_thread};1H   my_thread {MIKADO}{my_thread:2d}{RESET}   status {MIKADO}{process.status()}{RESET}  affinity {MIKADO}{affinity}{RESET}  pid {MIKADO}{pid:>6d}{RESET}   memory use: {MIKADO}{100*memoryUse:3.1f}{RESET}%   {RESTORE_CURSOR}{RESET}')
+  print( f'{SAVE_CURSOR}{CLEAR_LINE}{RESET}\r\033[{my_thread};1H   my_thread {MIKADO}{my_thread:2d}{RESET}   status {MIKADO}{process.status()}{RESET}  affinity {MIKADO}{affinity}{RESET}  pid {MIKADO}{pid:>6d}{RESET}   memory use: {MIKADO}{100*memoryUse:3.1f}{RESET}%   {RESTORE_CURSOR}{RESET}')
   
   x = {i for i in range(num_cpus)}
   os.sched_setaffinity( pid, x)
