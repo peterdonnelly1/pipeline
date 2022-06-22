90b928e9a96942de45059483effc3db707f94142
peterdonnelly1 <pd@red.com.au>
2022-06-22 10:01:45 +1000

    trivial formatting - SEEMS OK

--- classi/classify.py
+++ classi/classify.py
@@ -3495,9 +3495,9 @@ def test( cfg, args, parameters, embeddings_accum, labels_accum, epoch, test_loa
               image_tile_width           = args.supergrid_size**2*batch_size
 
               if DEBUG>0:
-                print ( f"CLASSI:         INFO:      test:             i                                                              = {BLEU}{i}{RESET}{CLEAR_LINE}"                                    )
                 print ( f"CLASSI:         INFO:      test:             supergrid_size                                                 = {BLEU}{args.supergrid_size}{RESET} hence image dimensions (tiles*tiles*batch_size = {args.supergrid_size}*{args.supergrid_size}*{batch_size}={image_tile_width}) will be {BLEU}{image_tile_width}x{image_tile_width}{RESET}{CLEAR_LINE}"  )
               if DEBUG>90:
+                print ( f"CLASSI:         INFO:      test:             i                                                              = {BLEU}{i}{RESET}{CLEAR_LINE}"                                    )
                 print ( f"CLASSI:         INFO:      test:             grid_labels.shape                                              = {BLEU}{grid_labels.shape}{RESET}{CLEAR_LINE}"                    )
                 print ( f"CLASSI:         INFO:      test:             grid_preds.shape                                               = {BLEU}{grid_preds.shape}{RESET}{CLEAR_LINE}"                     )
                 print ( f"CLASSI:         INFO:      test:             grid_p_highest.shape                                           = {BLEU}{grid_p_highest.shape}{RESET}{CLEAR_LINE}"                 )            
@@ -3515,7 +3515,7 @@ def test( cfg, args, parameters, embeddings_accum, labels_accum, epoch, test_loa
               grid_p_full_softmax_matrix = np.append( grid_p_full_softmax_matrix, p_full_softmax_matrix,      axis=0 )
 
 
-              if DEBUG>00:
+              if DEBUG>90:
                 print ( f"CLASSI:         INFO:      test:             i                                                              = {MIKADO}{i}{RESET}"                                                   )
                 print ( f"CLASSI:         INFO:      test:             supergrid_size                                                 = {MIKADO}{args.supergrid_size}{RESET} hence image dimensions (tiles*tiles*batch_size = {args.supergrid_size}*{args.supergrid_size}*{batch_size}={image_tile_width}) will be {BLEU}{image_tile_width}x{image_tile_width}{RESET}{CLEAR_LINE}"  )
                 print ( f"CLASSI:         INFO:      test:             grid_labels.shape                                              = {MIKADO}{grid_labels.shape}{RESET}"                                   )
