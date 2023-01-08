1a389286ca8fc4a416632479e0761d35030ebbc4
Peter <pd@red.com.au>
2023-01-08 14:45:20 +1100

    added environment variable to docker run command so that it will adopt the same timezone as the host ('TZ') - SEEMS OK

--- ____RUN_ME_FIRST_TO_BUILD_AND_RUN_THE_CLASSI_DOCKER_ENVIRONMENT.sh
+++ ____RUN_ME_FIRST_TO_BUILD_AND_RUN_THE_CLASSI_DOCKER_ENVIRONMENT.sh
@@ -5,8 +5,8 @@ set -x
 clear
 sudo docker container stop
 sudo docker container rm      classi
-sudo docker images    rmi -f  classi
+sudo docker           rmi -f  classi
 sudo DOCKER_BUILDKIT=1 docker build --progress=plain  -t classi .
 clear
 #sudo docker run -it --name classi --gpus device=0  --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --shm-size 2g   classi:latest
-sudo docker run -it --name classi --gpus device=0  --network=host -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --shm-size 2g -v /home/peter/git/pipeline/working_data:/home/peter/git/pipeline/working_data -v /home/peter/git/pipeline/source_data:/home/peter/git/pipeline/source_data   classi:latest
+sudo docker run -it --name classi --env TZ=$(cat /etc/timezone)  --gpus device=0  --network=host --shm-size 2g -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v /home/peter/git/pipeline/working_data:/home/peter/git/pipeline/working_data -v /home/peter/git/pipeline/source_data:/home/peter/git/pipeline/source_data  classi:latest
