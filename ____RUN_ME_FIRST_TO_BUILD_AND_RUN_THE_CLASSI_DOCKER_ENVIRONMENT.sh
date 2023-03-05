546f3fbfa20cd188dfaa85b2a94becbd5506598b
Peter <pd@red.com.au>
2023-03-05 11:30:57 +1100

    commennted out defunct code in pc_generate used only for analyse_data, which was causing issues because of increase in number of TCGA genes, but which is regardless unnecessary - NEEDS MORE REGRESSION TESTING - GOOD

--- ____RUN_ME_FIRST_TO_BUILD_AND_RUN_THE_CLASSI_DOCKER_ENVIRONMENT.sh
+++ ____RUN_ME_FIRST_TO_BUILD_AND_RUN_THE_CLASSI_DOCKER_ENVIRONMENT.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-mkdir -p pipeline
+[ $(basename $(pwd)) != "pipeline" ] && mkdir -p pipeline
 cd pipeline 2>/dev/null || true
 mkdir -p working_data
 mkdir -p source_data
@@ -15,7 +15,6 @@ sudo docker container rm      classi
 sudo docker           rmi -f  classi
 #sudo DOCKER_BUILDKIT=1 docker build --no-cache --progress=plain  -t classi .
 sudo DOCKER_BUILDKIT=1 docker build            --progress=plain  -t classi .
-clear
 sudo xhost +local:docker
 #sudo docker run -it --name classi --env TZ=$(cat /etc/timezone)  --gpus device=0  --network=host --shm-size 2g -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:1  classi:latest
 #~ sudo docker run -it --name classi --env TZ=$(cat /etc/timezone)  --gpus device=0  --network=host --shm-size 2g -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:1 -v /home/peter/git/pipeline/working_data:/home/peter/git/pipeline/working_data -v /home/peter/git/pipeline/source_data:/home/peter/git/pipeline/source_data  classi:latest
